c5a73a5660adbed7147d35832fcecc7cd20f0d66
HelenShao <52927975+HelenShao@users.noreply.github.com>
2021-01-16 00:16:21 -0500

    Update pysr.py

--- pysr.py
+++ pysr.py
@@ -7,16 +7,21 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 from torchvision import transforms, utils, datasets
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, random_split, SubsetRandomSampler
+import corr_coef
 from pysr import pysr, best, get_hof
 
-# Load the data for symbolic regression
-bottleneck_2 = np.load("../Bottleneck_2/Study2/Bottleneck_2_data.npy")[:,11:13]  # two features in bottleneck
-norm_input = np.load("../Bottleneck_2/Study2/Bottleneck_2_data.npy")[:,0:11]     # input data from test loader
+# load input and bottleneck
+bottleneck_2 = np.load("Bottleneck_2_.npy")[:,11:13]
+norm_input = np.load("Bottleneck_2_data.npy")[:,0:11]
 
-# PYSR parameters
-property_names   = ["m_vir", "v_max", "v_rms", "Halo Radius", "Scale Radius", 
-                    "velocity", "Angular momentum", "Spin", "b_to_a", "c_to_a", "T_u"]
+# 11 input features are the x-values
+# 1 bottleneck feature = y-value
+# find y in terms of the 11 x-values
+
+####################################### INPUT ###################################
+property_names   = ["m_vir", "v_max", "v_rms", "r_vir", "r_s", 
+                    "V", "J", "Spin", "b_to_a", "c_to_a", "T_u"]
 binary_operators = ["plus", "mult", "sub", "pow", "div"]
 unary_operators  = ["exp", "sin", "neg", "square", "cube", "logm", "logm10", "sqrtm", 
                     "sin", "cos", "tan"]
@@ -28,4 +33,3 @@ batch_size = 64       # for annealing and mutating
 # Initiate Symbolic regression with PYSR
 equations = pysr(x, y, niterations=500, binary_operators= binary_operators,
                  unary_operators= unary_operators, variable_names = property_names)
-
